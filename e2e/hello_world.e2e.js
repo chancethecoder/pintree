959e9b345f719ecf29376d436c79a6aef32ebc45
chancethecoder <chancethecoder@gmail.com>
2016-12-21 18:02:53 +0900

    Change e2e file

--- e2e/hello_world.e2e.js
+++ e2e/hello_world.e2e.js
@@ -6,10 +6,4 @@ describe('application launch', function () {
     beforeEach(testUtils.beforeEach);
     afterEach(testUtils.afterEach);
 
-    it('no test code here', function () {
-        // end to end test code here
-        return function() {
-            done();
-        };
-    });
 });
